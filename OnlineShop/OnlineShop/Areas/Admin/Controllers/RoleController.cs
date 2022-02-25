98e4254dd05593f053ba617a3183e5c69ca0bef3
Md. Hasibul Islam <45915540+hasib999@users.noreply.github.com>
2022-02-26 01:07:08 +0600

    role Crud Complete

--- OnlineShop/OnlineShop/Areas/Admin/Controllers/RoleController.cs
+++ OnlineShop/OnlineShop/Areas/Admin/Controllers/RoleController.cs
@@ -31,10 +31,82 @@ namespace OnlineShop.Areas.Customer.Controllers
         {
             IdentityRole identityRole = new IdentityRole();
             identityRole.Name = name;
+            var isExist = await _roleManager.RoleExistsAsync(identityRole.Name);
+            if(isExist)
+            {
+                ViewBag.message = "This Role is Already Exist";
+                ViewBag.name = name;
+                return View();
+            }
             var result = await _roleManager.CreateAsync(identityRole);
             if (result.Succeeded)
             {
                 TempData["save"] = "Role Saved Successfully";
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> Edit(string id)
+        {
+            var role = await _roleManager.FindByIdAsync(id);
+            if(role == null)
+            {
+                return NotFound();
+            }
+            ViewBag.id = role.Id;
+            ViewBag.name = role.Name;
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(string name,string id)
+        {
+            var role = await _roleManager.FindByIdAsync(id);
+            if (role == null)
+            {
+                return NotFound();
+            }
+            role.Name = name;
+            var isExist = await _roleManager.RoleExistsAsync(role.Name);
+            if (isExist)
+            {
+                ViewBag.message = "This Role is Already Exist";
+                ViewBag.name = name;
+                return View();
+            }
+            var result = await _roleManager.UpdateAsync(role);
+            if (result.Succeeded)
+            {
+                TempData["update"] = "Role Updated Successfully";
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> Delete(string id)
+        {
+            var role = await _roleManager.FindByIdAsync(id);
+            if (role == null)
+            {
+                return NotFound();
+            }
+            ViewBag.id = role.Id;
+            ViewBag.name = role.Name;
+            return View();
+        }
+        [HttpPost]
+        [ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirm(string id)
+        {
+            var role = await _roleManager.FindByIdAsync(id);
+            if (role == null)
+            {
+                return NotFound();
+            }
+            var result = await _roleManager.DeleteAsync(role);
+            if (result.Succeeded)
+            {
+                TempData["delete"] = "Role Deleted Successfully";
                 return RedirectToAction(nameof(Index));
             }
             return View();
